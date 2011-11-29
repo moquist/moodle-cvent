fea7bcc7c1f9132193a0492bc203a7f0b80b6284
Matt Oquist <moquist@majen.net>
2011-11-29 00:48:29 -0500

    update comments to document latestupdate feature of enrol_cvent_sync.php

--- moodle-1.9/enrol/cvent/enrol_cvent_sync.php
+++ moodle-1.9/enrol/cvent/enrol_cvent_sync.php
@@ -1,5 +1,15 @@
 <?php // $Id$
 
+# Call this from the command line to do regular synchornization. This is also 
+# called when a Moodle admin clicks the synchronize-now link on the config page.
+#
+# From the CLI, pass '--latestupdate null' to re-fetch all Cvent data without 
+# limiting to updates since the most recent synchronization.
+#
+# Using your browser, pass latestupdate=null to accomplish the same thing. The 
+# synchronize-now link on the config page passes this param by default to 
+# provide an easy way to re-pull all Cvent data.
+
     require_once(dirname(dirname(dirname(__FILE__))) . "/config.php");
     global $CFG;
     require_once("$CFG->libdir/accesslib.php");
